8d72a4b1067994b4bc8310eb9c038f27e93d443a
Amir-A-23 <amir.a.23@hotmail.com>
2022-02-11 13:57:49 -0600

    Add logic to display correct guesses

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const tileDisplay = document.querySelector('.tile-container');
 const keyboard = document.querySelector('.key-container');
-
+const messageDisplay = document.querySelector('.message-container');
+const testWeirdle = 'SUPER';
 const keys = [
 	'Q',
 	'W',
@@ -32,7 +33,7 @@ const keys = [
 	'«',
 ];
 
-const guestRows = [
+const guessRows = [
 	['', '', '', '', ''],
 	['', '', '', '', ''],
 	['', '', '', '', ''],
@@ -41,14 +42,17 @@ const guestRows = [
 	['', '', '', '', ''],
 ];
 
-guestRows.forEach((guestRow, guessRowIndex) => {
+let currentRow = 0;
+let currentTile = 0;
+
+guessRows.forEach((guessRow, guessRowIndex) => {
 	const rowElement = document.createElement('div');
-	rowElement.setAttribute('id', 'guestRow-' + guessRowIndex);
-	guestRow.forEach((guess, guessIndex) => {
+	rowElement.setAttribute('id', 'guessRow-' + guessRowIndex);
+	guessRow.forEach((guess, guessIndex) => {
 		const tileElement = document.createElement('div');
 		tileElement.setAttribute(
 			'id',
-			'guessRow-' + guessRowIndex + 'tile-' + guessIndex,
+			'guessRow-' + guessRowIndex + '-tile-' + guessIndex,
 		);
 		tileElement.classList.add('tile');
 		rowElement.append(tileElement);
@@ -56,13 +60,64 @@ guestRows.forEach((guestRow, guessRowIndex) => {
 	tileDisplay.append(rowElement);
 });
 
-const handleClick = () => {
-	console.log('clicked');
-};
 keys.forEach((key) => {
 	const buttonElement = document.createElement('button');
 	buttonElement.textContent = key;
 	buttonElement.setAttribute('id', key);
-	buttonElement.addEventListener('click', handleClick);
+	buttonElement.addEventListener('click', () => handleClick(key));
 	keyboard.append(buttonElement);
 });
+
+const handleClick = (letter) => {
+	console.log('clicked', letter);
+	if (letter === '«') {
+		deleteLetter();
+		return;
+	}
+	if (letter === 'ENTER') {
+		checkRow();
+		return;
+	}
+	addLetter(letter);
+};
+
+const addLetter = (letter) => {
+	if (currentTile < 5 && currentRow < 6) {
+		const tile = document.getElementById(
+			'guessRow-' + currentRow + '-tile-' + currentTile,
+		);
+		tile.textContent = letter;
+		guessRows[currentRow][currentTile] = letter;
+		tile.setAttribute('data', letter);
+		currentTile++;
+	}
+};
+
+const deleteLetter = () => {
+	if (currentTile > 0) {
+		currentTile--;
+		const tile = document.getElementById(
+			'guessRow-' + currentRow + '-tile-' + currentTile,
+		);
+		tile.textContent = '';
+		guessRows[currentRow][currentTile] = '';
+		tile.setAttribute('data', '');
+	}
+};
+
+const checkRow = () => {
+	const guess = guessRows[currentRow].join('');
+
+	if (currentTile === 5) {
+		if (testWeirdle == guess) {
+			showMessage('Magnificent!');
+		}
+	}
+};
+
+const showMessage = (message) => {
+	const messageElement = document.createElement('p');
+	messageElement.textContent = message;
+	messageDisplay.append(messageElement);
+	setTimeout(() => messageDisplay.removeChild(messageElement), 2000);
+};
